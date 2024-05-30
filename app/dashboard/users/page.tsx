3476ac081c543affe3ea6afdb1d046fcf9006a78
ABOLFAZL <abolfazlhasanzadeh83@gmail.com>
2024-05-30 23:00:57 +0330

    users page ui done

--- app/dashboard/users/page.tsx
+++ app/dashboard/users/page.tsx
@@ -1,3 +1,45 @@
+"use client"
+import React from "react";
+import {Table, TableHeader, TableColumn, TableBody, TableRow, TableCell, Chip} from "@nextui-org/react";
+import { MdEdit } from "react-icons/md";
+
+
 export default function Users() {
-  return <h1>یوزر ها</h1>;
+  return (
+<div className="w-8/12 flex flex-row mx-auto py-10 ">
+<Table removeWrapper    aria-label="Example static collection table">
+      <TableHeader >
+        <TableColumn className="text-lg" style={{borderRadius:"0 10px 10px 0", textAlign:"start"}}>وضعیت</TableColumn>
+        <TableColumn className="text-lg" style={{textAlign:"start"}}>آیدی</TableColumn>
+        <TableColumn className="text-lg" style={{textAlign:"start"}}>نام اکانات</TableColumn>
+        <TableColumn className="text-lg" style={{textAlign:"start"}}>نوع اکانت</TableColumn>
+        <TableColumn className="text-lg" style={{textAlign:"start"}}>حجم کل (GB)</TableColumn>
+        <TableColumn className="text-lg" style={{textAlign:"start"}}>حجم مصرفی (GB)</TableColumn>
+        <TableColumn className="text-lg" style={{borderRadius:"10px 0 0 10px",textAlign:"start"}}>ادیت</TableColumn>
+      </TableHeader>
+      <TableBody  >
+        <TableRow key="1">
+          <TableCell><Chip color="success" size="md" variant="flat">فعال</Chip></TableCell>
+          <TableCell>1055</TableCell>
+          <TableCell>ali</TableCell>
+          <TableCell>تک سرور</TableCell>
+          <TableCell>50</TableCell>
+          <TableCell>10.8</TableCell>
+          <TableCell><button className="p-2 text-2xl mx-3 bg-[#415FEF] rounded-lg "><MdEdit  /></button></TableCell>
+        </TableRow>
+        <TableRow key="2">
+        <TableCell><Chip color="danger" size="md" variant="flat">غیر فعال</Chip></TableCell>
+          <TableCell>1011</TableCell>
+          <TableCell>ali</TableCell>
+          <TableCell>سابسکریپشن</TableCell>
+          <TableCell>60</TableCell>
+          <TableCell>10</TableCell>
+          <TableCell><button className="p-2 text-2xl mx-3 bg-[#415FEF] rounded-lg "><MdEdit  /></button></TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+
+
+</div>
+  )
 }
