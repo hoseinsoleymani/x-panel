07385c535eacb91608b20b69715a7a557db04e02
ABOLFAZL <abolfazlhasanzadeh83@gmail.com>
2024-06-02 16:29:45 +0330

    setting page ui

--- app/admin/setting/page.tsx
+++ app/admin/setting/page.tsx
@@ -1,3 +1,76 @@
+import { Label } from '@/app/components/shared';
+import { Button, Input } from '@nextui-org/react';
+
 export default function Setting() {
-  return <h1>setting</h1>;
+  return (
+    <section className="w-full p-3">
+      <h1 className="text-xl">تنظمات پنل و نماینده ها:</h1>
+      <div className="grid grid-cols-3 gap-x-10 gap-y-20 p-5 ">
+        <div className="flex items-center">
+          <span>حداقل حجم برای ساخت اکانت</span>
+          <Input
+            color="secondary"
+            type="number"
+            label="براساس گیگابایت"
+            placeholder="حداقل حجم"
+            defaultValue="10"
+          />
+        </div>
+        <div className="flex items-center">
+          <span>حداکثر حجم برای ساخت اکانت</span>
+          <Input
+            color="secondary"
+            type="number"
+            label="براساس گیگابایت"
+            placeholder="حداقل حجم"
+            defaultValue="250"
+          />
+        </div>
+        <div className="flex items-center">
+          <span>حداقل مدت زمان برای ساخت اکانت</span>
+          <Input
+            color="secondary"
+            type="number"
+            label="براساس روز"
+            placeholder="حداقل حجم"
+            defaultValue="30"
+          />
+        </div>
+        <div className="flex items-center">
+          <span>حداکثر مدت زمان برای ساخت اکانت</span>
+          <Input
+            color="secondary"
+            type="number"
+            label="براساس روز"
+            placeholder="حداکثر زمان"
+            defaultValue="60"
+          />
+        </div>
+        <div className="flex items-center">
+          <span>حداکثر تعداد ایپی لیمیت برای ساخت اکانت</span>
+          <Input
+            color="secondary"
+            type="number"
+            label="تعداد یوزر"
+            placeholder="حداکثر لیمیت یوزر"
+            defaultValue="5"
+          />
+        </div>
+        <div className="flex items-center">
+          <span>api درگاه پرداخت نکست پی را وارد کنید</span>
+          <Input
+            color="secondary"
+            type="text"
+            label="api درگاه "
+            // placeholder="حداکثر لیمیت یوزر"
+          />
+        </div>
+      </div>
+      <div className=" flex p-7">
+        <Button className="mx-auto text-xl" color="warning">
+          ذخیره تغییرات
+        </Button>
+      </div>
+    </section>
+  );
 }
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <body className={`bg-[#060B26] text-slate-200 ${vazirfont.className}`}>
+    <body
+      className={`bg-[#060B26] h-[100vh] text-slate-200 ${vazirfont.className}`}
+    >
       <Header />
       <main className="justify-between p-5 md:flex md:flex-row xl:px-20 ">
         <Sidebar />
