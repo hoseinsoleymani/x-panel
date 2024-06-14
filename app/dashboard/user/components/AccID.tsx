e2580ed8f3f4884fcf12330dedc3f4057043c4f8
ABOLFAZL <abolfazlhasanzadeh83@gmail.com>
2024-06-14 21:21:40 +0330

    fix user page

---
+++ app/dashboard/user/components/AccID.tsx
@@ -0,0 +1,24 @@
+import { Card, CardBody } from '@nextui-org/react';
+import React from 'react';
+
+export default function AccID({ id,name }: any) {
+  return (
+    <div className=" flex items-center  gap-5 py-5">
+      <h1 className="text-2xl text-white">مشخصات:</h1>
+      <Card className=" bg-[#d4d4d8]">
+        <CardBody>
+          <p className="inline-block w-full justify-start px-1 text-xl  font-medium">
+            یوزر: {id}
+          </p>
+        </CardBody>
+      </Card>
+      <Card className="bg-[#d4d4d8]">
+        <CardBody>
+          <p className="justify-start px-1 text-xl font-medium">
+            نام اکانت: {name}
+          </p>
+        </CardBody>
+      </Card>
+    </div>
+  );
+}
