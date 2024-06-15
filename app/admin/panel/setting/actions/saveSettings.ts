c1a50021352af8d8b268c8639b035913e407d678
hoseinsoleymani <hoseinsolleymani@gmail.com>
2024-06-15 17:42:08 +0330

    feat: added settings

---
+++ app/admin/panel/setting/actions/saveSettings.ts
@@ -0,0 +1,31 @@
+'use server';
+
+import { z } from 'zod';
+
+import dbConnect from '../../../../api/connect-db';
+import Setting from '../../../../api/models/setting';
+import withValidation from '../../../../utils/zodValidation';
+
+const schema = z.object({});
+
+export const saveSetting = withValidation(
+  schema,
+  async (formData: FormData) => {
+    const entries = Object.entries(Object.fromEntries(formData.entries()));
+    const filterData = entries
+      .filter(([_, value]) => value !== '')
+      .filter(([key]) => !key.startsWith('$ACTION'));
+
+    try {
+      await dbConnect();
+    } catch (error: any) {
+      console.log(error);
+    }
+
+    try {
+      await Setting.updateMany({ ...Object.fromEntries(filterData) });
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+);
--- app/admin/panel/setting/page.tsx
+++ app/admin/panel/setting/page.tsx
@@ -1,76 +1,93 @@
-import { Label } from '@/app/components/shared';
+'use client';
+
 import { Button, Input } from '@nextui-org/react';
+import { useFormState } from 'react-dom';
+
+import { Label } from '@/app/components/shared';
+import { Card } from '@/app/components/shared/Card';
+
+import { saveSetting } from './actions/saveSettings';
+
+const initialMessage = {
+  message: '',
+};
 
 export default function Setting() {
+  const [state, formAction] = useFormState(saveSetting, initialMessage);
+
   return (
     <section className="w-full p-3">
-      <h1 className="text-xl">تنظمات پنل و نماینده ها:</h1>
-      <div className="grid xl:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-10 gap-y-10 p-5 ">
-        <div className="flex items-center">
-          <span>حداقل حجم برای ساخت اکانت</span>
-          <Input
-            color="secondary"
-            type="number"
-            label="براساس گیگابایت"
-            placeholder="حداقل حجم"
-            defaultValue="10"
-          />
-        </div>
-        <div className="flex items-center">
-          <span>حداکثر حجم برای ساخت اکانت</span>
-          <Input
-            color="secondary"
-            type="number"
-            label="براساس گیگابایت"
-            placeholder="حداقل حجم"
-            defaultValue="250"
-          />
-        </div>
-        <div className="flex items-center">
-          <span>حداقل مدت زمان برای ساخت اکانت</span>
-          <Input
-            color="secondary"
-            type="number"
-            label="براساس روز"
-            placeholder="حداقل حجم"
-            defaultValue="30"
-          />
-        </div>
-        <div className="flex items-center">
-          <span>حداکثر مدت زمان برای ساخت اکانت</span>
-          <Input
-            color="secondary"
-            type="number"
-            label="براساس روز"
-            placeholder="حداکثر زمان"
-            defaultValue="60"
-          />
-        </div>
-        <div className="flex items-center">
-          <span>حداکثر تعداد ایپی لیمیت برای ساخت اکانت</span>
-          <Input
-            color="secondary"
-            type="number"
-            label="تعداد یوزر"
-            placeholder="حداکثر لیمیت یوزر"
-            defaultValue="5"
-          />
-        </div>
-        <div className="flex items-center">
-          <span>api درگاه پرداخت نکست پی را وارد کنید</span>
-          <Input
-            color="secondary"
-            type="text"
-            label="api درگاه "
-            // placeholder="حداکثر لیمیت یوزر"
-          />
-        </div>
-      </div>
-      <div className=" flex p-7">
-        <Button className="mx-auto text-xl" color="warning">
-          ذخیره تغییرات
-        </Button>
-      </div>
+      <h1 className="text-xl text-white">تنظمات پنل و نماینده ها:</h1>
+      <Card>
+        <form
+          action={formAction}
+          className="grid grid-cols-1 gap-10 p-5 sm:grid-cols-2 xl:grid-cols-3"
+        >
+          <div className="flex flex-col items-start">
+            <Label>حداقل حجم برای ساخت اکانت</Label>
+            <Input
+              type="number"
+              label="براساس گیگابایت"
+              placeholder="حداقل حجم"
+              defaultValue="10"
+              name="minTraffic"
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <Label>حداکثر حجم برای ساخت اکانت</Label>
+            <Input
+              type="number"
+              label="براساس گیگابایت"
+              placeholder="حداقل حجم"
+              defaultValue="250"
+              name="maxTraffic"
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <Label>حداقل مدت زمان برای ساخت اکانت</Label>
+            <Input
+              type="number"
+              label="براساس روز"
+              placeholder="حداقل حجم"
+              defaultValue="30"
+              name="minExpirationTime"
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <Label>حداکثر مدت زمان برای ساخت اکانت</Label>
+            <Input
+              type="number"
+              label="براساس روز"
+              placeholder="حداکثر زمان"
+              defaultValue="60"
+              name="maxExpirationTime"
+            />
+          </div>
+          <div className="flex flex-col items-start">
+            <Label>حداکثر تعداد ایپی لیمیت برای ساخت اکانت</Label>
+            <Input
+              type="number"
+              label="تعداد یوزر"
+              placeholder="حداکثر لیمیت یوزر"
+              defaultValue="5"
+              name="maxUserLimit"
+            />
+          </div>
+          {/* <div className="flex flex-col items-start">
+            <Label>api درگاه پرداخت نکست پی را وارد کنید</Label>
+            <Input
+              type="text"
+              label="api درگاه "
+              // placeholder="حداکثر لیمیت یوزر"
+            />
+          </div> */}
+          <div className=" flex p-7">
+            <Button type="submit" className="mx-auto text-lg" color="primary">
+              ذخیره تغییرات
+            </Button>
+          </div>
+        </form>
+      </Card>
     </section>
   );
 }
