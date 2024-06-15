07599b385990d506d7c62760cb287bbb5642bdc3
ABOLFAZL <abolfazlhasanzadeh83@gmail.com>
2024-06-15 22:23:28 +0300

    fix

--- app/dashboard/create/components/AccountName.tsx
+++ app/dashboard/create/components/AccountName.tsx
@@ -1,14 +1,28 @@
 import { Input } from '@nextui-org/react';
+import React from 'react';
 
 import { Label } from '../../../components/shared';
 
 const AccountName = () => {
+  const [value, setValue] = React.useState("");
+
+  const validateEmail = (value) => value.match(/^[a-zA-Z]{3,}[a-zA-Z0-9]*$/i);
+
+  const isInvalid = React.useMemo(() => {
+    if (value === "") return false;
+
+    return validateEmail(value) ? false : true;
+  }, [value]);
   return (
     <div>
       <Label>اسم اکانت</Label>
       <div className="relative flex items-center">
         <Input
+          value={value}
+          onValueChange={setValue}
           className="text-white"
+          isInvalid={isInvalid}
+          color={isInvalid ? 'danger' : 'success'}
           name="account-name"
           variant="faded"
           label="اسم اکانت"
